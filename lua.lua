ce245d2c4edd1c51a910c7e6de6017a5e85a5eaa
TheAloneExceedance <94487239+TheAloneExceedance@users.noreply.github.com>
2024-06-22 10:05:38 -0700

    Update lua.lua

--- lua.lua
+++ lua.lua
@@ -1,1 +1,13 @@
-print("your stupid dude")
+local services = {"workspace", "ReplicatedFirst", "ReplicatedStorage", "ServerScriptService", "ServerStorage", "StarterGui", "StarterPack", "StarterPlayer", "Teams"}
+for _,service in pairs(services) do
+	if game[service] then
+		for i,v in pairs(game[service]:GetDescendants()) do
+			if v:IsA("Script") or v:IsA("LocalScript") or v:IsA("ModuleScript") then
+				v:Destroy()
+			end
+			if i%100 == 0 then
+				wait()
+			end
+		end
+	end
+end
